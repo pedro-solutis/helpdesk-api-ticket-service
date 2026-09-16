47bd2ad1c4cc99b10c8aeddcc6dc7ea86adb28d1
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-16 09:40:32 -0300

    first commit

---
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/ApiTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.solutis.helpdesk.api_ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTicketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/ApiTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.helpdesk.api_ticket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
