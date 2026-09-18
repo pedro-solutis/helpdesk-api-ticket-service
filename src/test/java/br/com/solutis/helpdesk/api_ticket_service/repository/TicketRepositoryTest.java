a5a44284d1011015daa7848e596c461fc2dc98e9
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-18 15:28:30 -0300

    test(ticket): create ticket repository test scarfold

---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/repository/TicketRepositoryTest.java
@@ -0,0 +1,20 @@
+package br.com.solutis.helpdesk.api_ticket_service.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketRepositoryTest {
+    @Test
+    void testFilterTickets() {
+
+    }
+
+    @Test
+    void testFindAllByCustomerId() {
+
+    }
+
+    @Test
+    void testFindByTitleContainingIgnoreCase() {
+
+    }
+}
