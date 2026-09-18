38d6ddf6a9d9fde924b63fc69ea0560165d0bb7e
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-18 15:28:17 -0300

    test(ticket): create ticket controller test scarfold

---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/controller/TicketControllerTest.java
@@ -0,0 +1,55 @@
+package br.com.solutis.helpdesk.api_ticket_service.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketControllerTest {
+    @Test
+    void testAssignTechnician() {
+
+    }
+
+    @Test
+    void testCloseTicket() {
+
+    }
+
+    @Test
+    void testCreateTicket() {
+
+    }
+
+    @Test
+    void testDeleteTicket() {
+
+    }
+
+    @Test
+    void testFilterTickets() {
+
+    }
+
+    @Test
+    void testGetAllTickets() {
+
+    }
+
+    @Test
+    void testGetTicketById() {
+
+    }
+
+    @Test
+    void testSearchTicketByCustomerId() {
+
+    }
+
+    @Test
+    void testSearchTicketByTitle() {
+
+    }
+
+    @Test
+    void testUpdateTicket() {
+
+    }
+}
