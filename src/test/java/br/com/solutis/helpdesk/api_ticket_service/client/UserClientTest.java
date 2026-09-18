7aa84c447ca40685e3e304679e1d96a63bdfbdf6
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-18 15:29:32 -0300

    test(ticket): create user client test scaffold

---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/client/UserClientTest.java
@@ -0,0 +1,10 @@
+package br.com.solutis.helpdesk.api_ticket_service.client;
+
+import org.junit.jupiter.api.Test;
+
+public class UserClientTest {
+    @Test
+    void testValidateUserExists() {
+
+    }
+}
