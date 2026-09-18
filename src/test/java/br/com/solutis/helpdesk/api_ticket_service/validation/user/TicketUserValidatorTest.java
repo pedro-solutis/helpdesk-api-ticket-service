ce7a4a456ed9d5dd59a4397a2ab4211f2d9dcc1a
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-18 15:29:50 -0300

    test(ticket): create ticket user validation test scaffold

---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/validation/user/TicketUserValidatorTest.java
@@ -0,0 +1,10 @@
+package br.com.solutis.helpdesk.api_ticket_service.validation.user;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketUserValidatorTest {
+    @Test
+    void testUserExist() {
+
+    }
+}
