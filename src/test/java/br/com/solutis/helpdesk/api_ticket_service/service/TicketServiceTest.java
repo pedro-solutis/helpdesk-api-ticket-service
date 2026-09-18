e04f7288f8cea91b68f5833613dd9ac04c2a6593
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-18 15:36:27 -0300

    test(ticket): create ticket service test scaffold

---
+++ src/test/java/br/com/solutis/helpdesk/api_ticket_service/service/TicketServiceTest.java
@@ -0,0 +1,57 @@
+package br.com.solutis.helpdesk.api_ticket_service.service;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketServiceTest {
+
+    @Test
+    public void testCreateTicket(){
+
+    }
+
+    @Test
+    public void testUpdateTicket(){
+
+    }
+
+    @Test
+    public void testAssignTechnician(){
+
+    }
+
+    @Test
+    public void testCloseTicket(){
+
+    }
+
+    @Test
+    public void testGetAllTicketByCustomerId(){
+
+    }
+
+    @Test
+    public void testGetAllTickets(){
+
+    }
+
+    @Test
+    public void testGetTicketById(){
+
+    }
+
+    @Test
+    public void testSearchTicketByTitle(){
+    }
+    
+    @Test
+    public void testFilterTickets(){
+
+    }
+
+    @Test
+    public void testDeleteTicket() {
+
+    }
+
+
+}
