c211e1155e444686912f2654d67f3885447d4d92
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-19 13:50:38 -0300

    feat(dto): add validation constraints to incoming request payloads

--- src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/AssignTechnicianDTO.java
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/AssignTechnicianDTO.java
@@ -1,9 +1,11 @@
 package br.com.solutis.helpdesk.api_ticket_service.dto.ticket;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record AssignTechnicianDTO(
     @NotNull(message = "The technician ID is required.")
+    @Positive(message = "The techincian ID must be greater than zero")
     Long technicianId
 ) {
 
--- src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/TicketUpdateDTO.java
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/TicketUpdateDTO.java
@@ -4,6 +4,7 @@ import br.com.solutis.helpdesk.api_ticket_service.model.Category;
 import br.com.solutis.helpdesk.api_ticket_service.model.Priority;
 import br.com.solutis.helpdesk.api_ticket_service.model.Status;
 import br.com.solutis.helpdesk.api_ticket_service.validation.enums.ValueOfEnum;
+import jakarta.validation.constraints.Size;
 
 public record TicketUpdateDTO(
     
@@ -13,6 +14,7 @@ public record TicketUpdateDTO(
     @ValueOfEnum (enumClass = Category.class, message = "Invalid category. AcceptedValue: SOFTWARE, HARDWARE, NETWORD.")
     String category,
     
+    @Size(min = 3)
     String description,
 
     @ValueOfEnum (enumClass = Status.class, message = "Invalid status. AcceptedValue: IN_PROGRESS, WAITING, RESOLVED.")
