4473ebe3e6895d0e4d5a0d15dc9f864b09feb665
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-21 01:05:40 -0300

    feat(security) add autorization parameter in swagger config

--- src/main/java/br/com/solutis/helpdesk/api_ticket_service/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/infra/springdoc/SpringDocConfiguration.java
@@ -3,10 +3,13 @@ package br.com.solutis.helpdesk.api_ticket_service.infra.springdoc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDocConfiguration {
@@ -14,6 +17,13 @@ public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI(){
         return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
             .info(new Info()
                 .title("solutis.helpdesk.ticket API")
                 .description("Ticket Service API Rest with CRUD, filters and search functionalities.")
@@ -26,5 +36,4 @@ public class SpringDocConfiguration {
             )
         );
     }
-
 }
