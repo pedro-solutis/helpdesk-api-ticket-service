af91fd7cdeb8db9251c15c9bc1c8ea76f4e53257
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-21 23:00:58 -0300

    refactor(config): externalize application settings to environment variables

--- src/main/java/br/com/solutis/helpdesk/api_ticket_service/ApiTicketServiceApplication.java
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/ApiTicketServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080/api"), @Server(url = "http://localhost:8082") })
+@OpenAPIDefinition(servers = { @Server(url = "${GATEWAY_URL:http://localhost:8080/api}", description = "Gateway API"), @Server(url = "${LOCAL_URL:http://localhost:8082}", description = "Local Server") })
 @SpringBootApplication
 @EnableFeignClients 
 public class ApiTicketServiceApplication {
