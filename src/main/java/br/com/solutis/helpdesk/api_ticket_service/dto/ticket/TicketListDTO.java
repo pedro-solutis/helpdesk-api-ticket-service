2d1ca6eec2a2fa2025c53a4876f8933089554f59
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-23 17:19:51 -0300

    feat(ticket): add new fields to ticket list response dto

--- src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/TicketListDTO.java
+++ src/main/java/br/com/solutis/helpdesk/api_ticket_service/dto/ticket/TicketListDTO.java
@@ -2,19 +2,20 @@ package br.com.solutis.helpdesk.api_ticket_service.dto.ticket;
 
 import java.time.LocalDateTime;
 
-import br.com.solutis.helpdesk.api_ticket_service.model.Status;
 import br.com.solutis.helpdesk.api_ticket_service.model.Ticket;
 
 public record TicketListDTO(
     Long id,
     String title,
-    Status status,
+    String status,
+    String priority,
+    String category,
     Long customerId,
     LocalDateTime createdAt) 
 {
 
     public TicketListDTO(Ticket ticket){
-        this(ticket.getId(), ticket.getTitle(), ticket.getStatus(), ticket.getCustomerId(),ticket.getCreatedAt());
+        this(ticket.getId(), ticket.getTitle(), ticket.getStatus().toString(), ticket.getPriority().toString(), ticket.getCategory().toString(), ticket.getCustomerId(),ticket.getCreatedAt());
     }
 
 }
